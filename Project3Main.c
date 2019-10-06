78d49689862bcc85eac3548b793d599d413e0071
Alexander Hughey <hughey@hoare7.cs.umsl.edu>
2019-10-06 18:53:49 -0500

    third

--- Project3Main.c
+++ Project3Main.c
@@ -1,33 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[])
 {
 
 	int opt, time;
+	int maxChildProcesses = 5;
+	int realTime = 5;
+	char* logFile;
 
-	while((opt = getopt(argc, argv, "hs:l:t:")) != -1)
+	while((opt = getopt(argc, argv, ":hs:l:t:")) != -1)
 	{
 		switch(opt)
 		{
 			case 'h':
-				printf("help\n");
+				printf("HELP\n");
+				printf("Options are: \n");
+				printf(" -s x		where x is an integer for maximum number of child processes spawned.\n");
+				printf(" -l filename	filename specifies the name for the log file.\n");
+				printf(" -t z		where z is the time in real seconds when the master will terminate itself and all children.\n");
 				break;
 			case 's':
-				printf("s\n");
+				printf("s with %s\n", optarg);
+				maxChildProcesses = optarg;
 				break;
 			case 'l':
-				printf("l\n");
+				printf("l with %s\n", optarg);
+				logFile = optarg;
 				break;
 			case 't':
-				printf("t\n");
+				printf("t with %s\n", optarg);
+				realTime = optarg;
 				break;
 			case ':':
 				printf("Option needs a value\n");
 				break;
 			case '?':
-				printf("Unknown option: \n");
+				printf("Unknown option: %c\n", optopt);
 				break;
 		}
 	}
+	for(; optind < argc; optind++)
+	{
+		printf("extra arguments: %s\n", argv[optind]);
+	}
+	return 0;
 }
